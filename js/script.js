d39e4911c328aa4435b86b8ba2694e6942ea8463
Wilhem Barbier <wilhem.b@free.fr>
2020-10-03 11:19:08 +0200

    it actually works now

--- js/script.js
+++ js/script.js
@@ -6,21 +6,24 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 // let geometry = new THREE.BoxGeometry();
-let geometry = new THREE.SphereGeometry();
+let geometry = new THREE.SphereGeometry(1, 32, 32);
 let material = new THREE.MeshBasicMaterial({color: 0x00ff00});
 material.onBeforeCompile = function(shader) {
     shader.vertexShader = document.getElementById('vertexShader').text;
     shader.fragmentShader = document.getElementById('fragmentShader').text;
 }
 let cube = new THREE.Mesh(geometry, material);
+let axes = new THREE.AxesHelper(30);
+cube.add(axes);
 scene.add(cube);
 
-camera.position.z = 5;
+camera.position.z = +5;
+// camera.rotation.y = 3.14;
 
 function animate() {
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
-    cube.rotation.x += .01;
-    cube.rotation.y += .01;
+    // cube.rotation.x += .01;
+    // cube.rotation.y += .01;
 }
 animate();
