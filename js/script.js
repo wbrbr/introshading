a32590b1fc7d483ae68e23404cc1506660893240
Wilhem Barbier <wilhem.b@free.fr>
2020-10-03 17:18:05 +0200

    syntax highlighting

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ let camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000);
 let container = document.getElementById('container');
 let renderer = new THREE.WebGLRenderer();
 // renderer.setSize(container.clientWidth, container.clientHeight);
-renderer.setSize(window.innerWidth / 3., window.innerWidth / 3);
+renderer.setSize(window.innerHeight / 2, window.innerHeight / 2);
 container.prepend(renderer.domElement);
 
 // let geometry = new THREE.BoxGeometry();
@@ -19,11 +19,11 @@ material.onBeforeCompile = function(shader) {
     shader.fragmentShader = editor.getValue()
 }
 let cube = new THREE.Mesh(geometry, material);
-let axes = new THREE.AxesHelper(30);
-cube.add(axes);
+// let axes = new THREE.AxesHelper(30);
+// cube.add(axes);
 scene.add(cube);
 
-camera.position.z = +5;
+camera.position.z = +2;
 
 
 function animate() {
@@ -39,3 +39,7 @@ document.getElementById('run').addEventListener('click', function() {
     material.fragmentShader = editor.getValue();
     material.needsUpdate = true;
 });
+
+window.addEventListener('resize', function() {
+    renderer.setSize(window.innerHeight / 2, window.innerHeight / 2);
+})
